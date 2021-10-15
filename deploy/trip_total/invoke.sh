38ecf9b7abd189a283c68927dc69019d3d77b906
Injenia <40032972+Injenia@users.noreply.github.com>
2021-10-15 17:12:23 +0200

    Update invoke.sh

--- deploy/trip_total/invoke.sh
+++ deploy/trip_total/invoke.sh
@@ -4,5 +4,5 @@ current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source ${current_dir}/config.sh
 
-curl -d '{"instances": [{"pickup_latitude": 0, "pickup_longitude": 0, "dropoff_latitude": 0, "dropoff_longitude": 0, "trip_start": "2017-03-03 Fry-09:00", "company": ""}], "signature_name": "predict"}' \
+curl -d '{"instances": [{"pickup_latitude": 0, "pickup_longitude": 0, "dropoff_latitude": 0, "dropoff_longitude": 0, "company": ""}], "signature_name": "predict"}' \
     -X POST https://${AI_PLATFORM_PREDICTION_REGION}-ml.googleapis.com/v1/projects/mlteam-ml-specialization-2021/models/${AI_PLATFORM_PREDICTION_MODEL}:predict?access_token\="$(gcloud auth application-default print-access-token)"
